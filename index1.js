d557c2f03950c4f3f1624073fac4d10b8ca4e7ed
Mohmmad kaif <mohdkaif783032@gmail.com>
2024-07-14 23:46:41 +0530

    First Commit

---
+++ index1.js
@@ -0,0 +1,8 @@
+let name="Mohdkaif";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.charAt());
+console.log(name.slice(3,5));
+console.log(name.concat());
+console.log(name.repeat());
---
+++ index2.js
@@ -0,0 +1,6 @@
+console.log("start");
+setTimeout(function(){
+    console.log("hey I am Mohmmad kaif");
+},3000
+)
+console.log("End");
